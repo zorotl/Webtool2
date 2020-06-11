f168ee32635ae4029f31b78c3929ae83a7f8f1a4
Martin Striednig <m.striednig@gmail.com>
2020-06-11 09:52:50 +0200

    form design new with border

--- resources/views/warehouse/item/condition/create.blade.php
+++ resources/views/warehouse/item/condition/create.blade.php
@@ -9,18 +9,20 @@
                 <h1 class="text-primary">Lager</h1>
                 <h2 class="h3 text-primary my-4">Artikel-Zustand hinzufügen</h2>
                 <form action="/itemCondition" method="post">
-                    @csrf
-                    <div class="form-group">
-                        <label for="zustand">Zustand</label>
-                        <input type="text" class="form-control {{ $errors->has('zustand') ? 'border-danger' : ''}}"
-                               id="zustand" name="zustand" value="{{ old('zustand') }}" autofocus>
-                        <small class="form-text text-danger">{!! $errors->first('zustand') !!}</small>
-                    </div>
-                    <input class="btn btn-primary mt-4" type="submit" value="Hinzufügen">
-                    <a class="btn btn-primary mt-4 float-lg-right" href="/itemCondition">
-                        <i class="fas fa-arrow-circle-up mr-2"></i>
-                        Zurück
-                    </a>
+                    <fieldset class="border border-secondary rounded-lg p-4">
+                        @csrf
+                        <div class="form-group">
+                            <label for="zustand">Zustand</label>
+                            <input type="text" class="form-control {{ $errors->has('zustand') ? 'border-danger' : ''}}"
+                                   id="zustand" name="zustand" value="{{ old('zustand') }}" autofocus>
+                            <small class="form-text text-danger">{!! $errors->first('zustand') !!}</small>
+                        </div>
+                        <input class="btn btn-primary mt-4" type="submit" value="Hinzufügen">
+                        <a class="btn btn-primary mt-4 float-lg-right" href="/itemCondition">
+                            <i class="fas fa-arrow-circle-up mr-2"></i>
+                            Zurück
+                        </a>
+                    </fieldset>
                 </form>
             </div>
         </div>
--- resources/views/warehouse/item/condition/edit.blade.php
+++ resources/views/warehouse/item/condition/edit.blade.php
@@ -9,19 +9,21 @@
                 <h1 class="text-primary">Lager</h1>
                 <h2 class="h3 text-primary my-4">Artikel-Zustand bearbeiten</h2>
                 <form action="/itemCondition/{{ $itemCondition->id }}" method="post">
-                    @csrf
-                    @method('PUT')
-                    <div class="form-group">
-                        <label for="zustand">Zustand</label>
-                        <input type="text" class="form-control {{ $errors->has('zustand') ? 'border-danger' : ''}}"
-                               id="zustand" name="zustand" value="{{ $itemCondition->zustand ?? old('zustand') }}" autofocus>
-                        <small class="form-text text-danger">{!! $errors->first('zustand') !!}</small>
-                    </div>
-                    <input class="btn btn-primary mt-4" type="submit" value="Änderung speichern">
-                    <a class="btn btn-primary mt-4 float-lg-right" href="/itemCondition">
-                        <i class="fas fa-arrow-circle-up mr-2"></i>
-                        Zurück
-                    </a>
+                    <fieldset class="border border-secondary rounded-lg p-4">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="zustand">Zustand</label>
+                            <input type="text" class="form-control {{ $errors->has('zustand') ? 'border-danger' : ''}}"
+                                   id="zustand" name="zustand" value="{{ $itemCondition->zustand ?? old('zustand') }}" autofocus>
+                            <small class="form-text text-danger">{!! $errors->first('zustand') !!}</small>
+                        </div>
+                        <input class="btn btn-primary mt-4" type="submit" value="Änderung speichern">
+                        <a class="btn btn-primary mt-4 float-lg-right" href="/itemCondition">
+                            <i class="fas fa-arrow-circle-up mr-2"></i>
+                            Zurück
+                        </a>
+                    </fieldset>
                 </form>
             </div>
         </div>
