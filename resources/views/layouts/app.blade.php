90e3e6cdc6b51ee45622f7df26d9ac4fe6fc7559
Martin Striednig <m.striednig@gmail.com>
2021-04-25 08:55:55 +0200

    nav => MeinKonto disabled and Login/Register disabled

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,52 +84,52 @@
                         <a class="nav-link {{ Request::is('links*') ? 'active' : '' }}" href="#">Links</a>
                     </li>
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle {{ Request::is('account*') ? 'active' : '' }}"
-                           href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Mein Konto
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown04">
-                            <a class="dropdown-item" href="#">Mein Konto</a>
-                            <a class="dropdown-item" href="#">User: Verwaltung</a>
-                            <a class="dropdown-item" href="#">Link: Verwaltung</a>
-                            <a class="dropdown-item" href="#">Website: Verwaltung</a>
-                        </div>
-                    </li>
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a class="nav-link dropdown-toggle {{ Request::is('account*') ? 'active' : '' }}"--}}
+{{--                           href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                            Mein Konto--}}
+{{--                        </a>--}}
+{{--                        <div class="dropdown-menu" aria-labelledby="dropdown04">--}}
+{{--                            <a class="dropdown-item" href="#">Mein Konto</a>--}}
+{{--                            <a class="dropdown-item" href="#">User: Verwaltung</a>--}}
+{{--                            <a class="dropdown-item" href="#">Link: Verwaltung</a>--}}
+{{--                            <a class="dropdown-item" href="#">Website: Verwaltung</a>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+{{--                <ul class="navbar-nav ml-auto">--}}
+{{--                    <!-- Authentication Links -->--}}
+{{--                    @guest--}}
+{{--                        <li class="nav-item">--}}
+{{--                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
+{{--                        </li>--}}
+{{--                        @if (Route::has('register'))--}}
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                            </li>--}}
+{{--                        @endif--}}
+{{--                    @else--}}
+{{--                        <li class="nav-item dropdown">--}}
+{{--                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                                {{ Auth::user()->name }} <span class="caret"></span>--}}
+{{--                            </a>--}}
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+{{--                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                                <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                                   onclick="event.preventDefault();--}}
+{{--                                                 document.getElementById('logout-form').submit();">--}}
+{{--                                    {{ __('Logout') }}--}}
+{{--                                </a>--}}
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+{{--                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+{{--                                    @csrf--}}
+{{--                                </form>--}}
+{{--                            </div>--}}
+{{--                        </li>--}}
+{{--                    @endguest--}}
+{{--                </ul>--}}
             </div>
         </nav>
 
