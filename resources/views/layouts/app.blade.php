f4bfd09ddda0078f3e4c5b46cf9b349af40ebc9f
Martin Striednig <m.striednig@gmail.com>
2021-04-30 20:40:26 +0200

    navbar new with container xl

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,121 +28,123 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
-            <a class="navbar-brand" href="{{ url('/home') }}">
-                <i class="fas fa-warehouse"></i>
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="container-xl">
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <i class="fas fa-warehouse"></i>
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle {{ Request::is('warehouse*') ? 'active' : '' }}"
-                           href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Lager
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <a class="dropdown-item" href="{{ route('item.index') }}">Artikel anzeigen</a>
-                            <a class="dropdown-item" href="{{ route('item.create') }}">Artikel hinzufügen</a>
-                            <a class="dropdown-item" href="{{ route('warehouse.index') }}">Lager</a>
-                            <a class="dropdown-item" href="{{ route('brand.index') }}">Marken</a>
-                            <a class="dropdown-item" href="{{ route('itemType.index') }}">Artikel-Arten</a>
-                            <a class="dropdown-item" href="{{ route('itemCondition.index') }}">Artikel-Zustände</a>
-                        </div>
-                    </li>
-
-{{--                    <li class="nav-item dropdown">--}}
-{{--                        <a class="nav-link dropdown-toggle {{ Request::is('shop*') ? 'active' : '' }}"--}}
-{{--                           href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                            Shop--}}
-{{--                        </a>--}}
-{{--                        <div class="dropdown-menu" aria-labelledby="dropdown02">--}}
-{{--                            <a class="dropdown-item" href="#">Suche</a>--}}
-{{--                            <a class="dropdown-item" href="#">Bestellungen</a>--}}
-{{--                            <a class="dropdown-item" href="#">Verwaltung</a>--}}
-{{--                            <a class="dropdown-item" href="#">Statistik</a>--}}
-{{--                            <a class="dropdown-item" href="#">History</a>--}}
-{{--                        </div>--}}
-{{--                    </li>--}}
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle {{ Request::is('calculator*') ? 'active' : '' }}"
-                           href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Rechner
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown03">
-                            <a class="dropdown-item" href="{{ route('calculator.index') }}">Rechner</a>
-                            <a class="dropdown-item" href="{{ route('calculator.edit', '1') }}">Konfiguration</a>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle {{ Request::is('links*') ? 'active' : '' }}"
-                           href="#" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Links
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown05">
-                            @foreach($links_nav as $link_nav)
-                                <a class="dropdown-item" target="_blank" href="{{ $link_nav->link_url }}">{{ $link_nav->link_name }}</a>
-                            @endforeach
-                            <a class="dropdown-item" href="{{ route('link.index') }}">Links bearbeiten</a>
-                        </div>
-                    </li>
-
-{{--                    <li class="nav-item dropdown">--}}
-{{--                        <a class="nav-link dropdown-toggle {{ Request::is('account*') ? 'active' : '' }}"--}}
-{{--                           href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                            Mein Konto--}}
-{{--                        </a>--}}
-{{--                        <div class="dropdown-menu" aria-labelledby="dropdown04">--}}
-{{--                            <a class="dropdown-item" href="#">Mein Konto</a>--}}
-{{--                            <a class="dropdown-item" href="#">User: Verwaltung</a>--}}
-{{--                            <a class="dropdown-item" href="#">Link: Verwaltung</a>--}}
-{{--                            <a class="dropdown-item" href="#">Website: Verwaltung</a>--}}
-{{--                        </div>--}}
-{{--                    </li>--}}
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link {{ Request::is('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
                         </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
+
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                            <a class="nav-link dropdown-toggle {{ Request::is('warehouse*') ? 'active' : '' }}"
+                               href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Lager
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
+                            <div class="dropdown-menu" aria-labelledby="dropdown01">
+                                <a class="dropdown-item" href="{{ route('item.index') }}">Artikel anzeigen</a>
+                                <a class="dropdown-item" href="{{ route('item.create') }}">Artikel hinzufügen</a>
+                                <a class="dropdown-item" href="{{ route('warehouse.index') }}">Lager</a>
+                                <a class="dropdown-item" href="{{ route('brand.index') }}">Marken</a>
+                                <a class="dropdown-item" href="{{ route('itemType.index') }}">Artikel-Arten</a>
+                                <a class="dropdown-item" href="{{ route('itemCondition.index') }}">Artikel-Zustände</a>
                             </div>
                         </li>
-                    @endguest
-                </ul>
+
+                        {{--                    <li class="nav-item dropdown">--}}
+                        {{--                        <a class="nav-link dropdown-toggle {{ Request::is('shop*') ? 'active' : '' }}"--}}
+                        {{--                           href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+                        {{--                            Shop--}}
+                        {{--                        </a>--}}
+                        {{--                        <div class="dropdown-menu" aria-labelledby="dropdown02">--}}
+                        {{--                            <a class="dropdown-item" href="#">Suche</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">Bestellungen</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">Verwaltung</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">Statistik</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">History</a>--}}
+                        {{--                        </div>--}}
+                        {{--                    </li>--}}
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle {{ Request::is('calculator*') ? 'active' : '' }}"
+                               href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Rechner
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown03">
+                                <a class="dropdown-item" href="{{ route('calculator.index') }}">Rechner</a>
+                                <a class="dropdown-item" href="{{ route('calculator.edit', '1') }}">Konfiguration</a>
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle {{ Request::is('links*') ? 'active' : '' }}"
+                               href="#" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Links
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown05">
+                                @foreach($links_nav as $link_nav)
+                                    <a class="dropdown-item" target="_blank" href="{{ $link_nav->link_url }}">{{ $link_nav->link_name }}</a>
+                                @endforeach
+                                <a class="dropdown-item" href="{{ route('link.index') }}">Links bearbeiten</a>
+                            </div>
+                        </li>
+
+                        {{--                    <li class="nav-item dropdown">--}}
+                        {{--                        <a class="nav-link dropdown-toggle {{ Request::is('account*') ? 'active' : '' }}"--}}
+                        {{--                           href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+                        {{--                            Mein Konto--}}
+                        {{--                        </a>--}}
+                        {{--                        <div class="dropdown-menu" aria-labelledby="dropdown04">--}}
+                        {{--                            <a class="dropdown-item" href="#">Mein Konto</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">User: Verwaltung</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">Link: Verwaltung</a>--}}
+                        {{--                            <a class="dropdown-item" href="#">Website: Verwaltung</a>--}}
+                        {{--                        </div>--}}
+                        {{--                    </li>--}}
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </nav>
 
