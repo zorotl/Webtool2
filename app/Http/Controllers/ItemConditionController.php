f2ae2cef8569db4b5d3337c9a976ea780b12ed85
Martin Striednig <m.striednig@gmail.com>
2021-05-01 12:39:51 +0200

    ItemCondition- and ItemType-Controller auth

--- app/Http/Controllers/ItemConditionController.php
+++ app/Http/Controllers/ItemConditionController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Session;
 
 class ItemConditionController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
